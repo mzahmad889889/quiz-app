1643addc685cd33d52fff79090c18db74e8a9c35
Syed Haris shah <haris.official222@gmail.com>
2025-12-09 10:10:24 +0500

    create quiz layout, some cahnges in UI

--- front-end/src/components/HomePage.jsx
+++ front-end/src/components/HomePage.jsx
@@ -5,9 +5,10 @@ function HomePage() {
 
     const navigate = useNavigate()
     return (
-        <div className='w-full font-system-ui h-screen bg-[#003d39] pt-6'>
-            <div className='h-18 bg-white rounded-3xl shadow-lg w-[90%] px-5 m-auto justify-between flex items-center'>
-                <h1 className='text-2xl text-[#003d39] font-bold font-mono '>QUIZ</h1>
+        <div className='w-full font-system-ui h-screen bg-[#003d39]'>
+            <div className='h-18 bg-white shadow-lg w-[100%] px-15 m-auto justify-between flex items-center'>
+
+                <a  href="/" className='text-2xl text-[#003d39] font-bold font-mono'>SMARTQUIZ</a>
                 <div className='flex items-center gap-4'>
                     <button
                         type='button'
@@ -19,19 +20,19 @@ function HomePage() {
                         className='w-25 h-10  cursor-pointer bg-[#003d39] text-white text-lg font-bold rounded-2xl hover:bg-[#74EE66] hover:text-black transition duration-200 ease-in'>Sign-Up</button></div>
             </div>
 
-            <div className='w-[90%] h-[80%] bg-white rounded-2xl flex item-center m-auto justify-center gap-10 mt-10 pt-10 pb-10'>
+            <div className='w-[100%] text-white h-[90%] bg-[#003d39] flex item-center m-auto justify-center gap-8 pt-10 pb-10'>
                 <div className='w-[50%] p-15 '>
-                    <h1 className='text-5xl font-semibold mb-5 text-[#003d39] '>Welcome to Online Quiz</h1>
+                    <h1 className='text-4xl font-semibold mb-5'>Welcome to  SmartQuiz</h1>
 
-                    <h2 className='text-4xl text-[#003d39] font-semibold mb-8 '>Test Your knowledge</h2>
+                    <h2 className='text-3xl font-semibold mb-6 '>Test Your knowledge</h2>
 
-                    <p className='text-lg text-[#003d39] leading-[2rem] mb-8'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga voluptatum aliquid officiis. Facere laboriosam blanditiis maiores sed? Fuga sit ipsam ducimus consequatur tempora saepe modi possimus nulla quis animi, non accusantium in sequi dolorum quas hic ab consectetur minima nesciunt?</p>
+                    <p className='text-lg leading-[2rem] mb-8'>Welcome to SmartQuiz — a personalized space designed to help you manage your learning effortlessly. Here, you can view all the quizzes assigned to you, check their details such time limit, and number of questions, and start any quiz with just one click. You can also see your completed quizzes and track your progress over time. SamrtQuiz is clean, simple, and focused on providing you with everything you need to stay prepared, stay organized, and perform your best. Ensuring a secure and fully personalized experience every time you use the app.</p>
 
-                    <button className='w-40 h-13 bg-[#003d39]  cursor-pointer text-xl font-bold rounded-xl text-white hover:bg-[#74EE66] hover:text-black  transition' > Learn More</button>
+                    <button className='w-40 h-13 bg-white cursor-pointer text-xl font-bold rounded-xl text-black hover:bg-[#74EE66] hover:text-[#003d39] transition duration-200 ease-in' > Learn More</button>
                 </div>
 
                 <div className='w-[58%]'>
-                    <img className='w-[93%] rounded-3xl shadow-xl' src="https://thumbs.dreamstime.com/b/illustration-online-test-concept-education-questionnaire-form-survey-metaphor-answering-internet-quiz-homework-assignment-362437988.jpg" alt="online-test pic" />
+                    <img className='w-[93%] mt-10 rounded-3xl shadow-xl' src="https://thumbs.dreamstime.com/b/illustration-online-test-concept-education-questionnaire-form-survey-metaphor-answering-internet-quiz-homework-assignment-362437988.jpg" alt="online-test pic" />
                 </div>
             </div>
 
